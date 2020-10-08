40db4e30ee12e137fe080dc2141e40b192903cd8
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-08 13:22:54 +0100

    Calculating Daily Employee Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 read -p "ENTER THE NUMBER OF EMPLOY" NUM
+ISFULLTIME=1
+WAGES_PER_HOUR=20
+DAILY_EMPLOY_WAGES=0
 for (( i=1;i<=$NUM;i++ ))
 do
 	ATTEND=$((RANDOM%2))
 	if [ $ATTEND -eq 1 ]
 	then
-		echo "EMPLOY $i PRESENT"
+		DAILY_EMPLOY_WAGES=$(($DAILY_EMPLOY_WAGES+$(($WAGES_PER_HOUR*$((RANDOM%9+1))))))
+		echo "WAGES OF EMPLOY $i:$DAILY_EMPLOY_WAGES"
 	else
 		echo "EMPLOY $i ABSENT"
 	fi
