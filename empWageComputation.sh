549809684d4ffc18b2307aa3f5c7d8a85284dd3e
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-08 12:52:21 +0100

    attendence check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+read -p "ENTER THE NUMBER OF EMPLOY" NUM
+for (( i=1;i<=$NUM;i++ ))
+do
+	ATTEND=$((RANDOM%2))
+	if [ $ATTEND -eq 1 ]
+	then
+		echo "EMPLOY $i PRESENT"
+	else
+		echo "EMPLOY $i ABSENT"
+	fi
+done
