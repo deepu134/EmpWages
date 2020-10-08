d54b8f2176705f39554ed821b81bad77275c07fe
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-08 22:57:55 +0100

    refactor to function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,25 +7,27 @@ TOTAL_EMPLOY_WAGES=0
 WORKING_DAYS=20
 TOTAL_WORKING_HOURS=100
 count=0
-for (( i=1;i<=$WORKING_DAYS;i++ ))
+function wages(){
+for (( i=1;i<=$1;i++ ))
 do
-	RAND_CHECK=$((RANDOM%3+1)
-	if [ $count -ne $TOTAL_WORKING_HOURS ]
+	RAND_CHECK=$((RANDOM%3+1))
+	if [ $count -ne $2 ]
 	then
 		case $RAND_CHECK in
-			$ISFULLTIME )TOTAL_EMPLOY_WAGES=$(($TOTAL_EMPLOY_WAGES+$(($WAGES_PER_HOUR*8))))
-			count=$(($count+8))
-			;;
-
-			$ISPARTTIME )TOTAL_EMPLOY_WAGES=$(($TOTAL_EMPLOY_WAGES+$(($WAGES_PER_HOUR*4))))
-			count=$(($count+4))
-			;;
+			$ISFULLTIME)
+			TOTAL_EMPLOY_WAGES=$(($TOTAL_EMPLOY_WAGES+$(($WAGES_PER_HOUR*8))))
+			count=$(($count+8));;
+			$ISPARTTIME)
+			TOTAL_EMPLOY_WAGES=$(($TOTAL_EMPLOY_WAGES+$(($WAGES_PER_HOUR*4))))
+			count=$(($count+4));;
 			*);;
 		esac
 	else
 		break
 	fi
 done
+}
+wages  $WORKING_DAYS $TOTAL_WORKING_HOURS
 echo $count
-echo "WAGES FOR FULLTIME :$TOTAL_EMPLOY_WAGES"
+echo "TOTAL WAGES :$TOTAL_EMPLOY_WAGES"
 #echo "WAGES FOR PARTIME::$DAILY_EMPLOY_WAGES_PART_TIME"
